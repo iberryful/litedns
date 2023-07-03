0af8b5c426a6d766891a6471e5348a17b0def96e
i349934 <berry.ban@sap.com>
2023-07-04 01:43:55 +0800

    feat: return no records when ipv6 prefer/defer is satisfied

--- src/handler.rs
+++ src/handler.rs
@@ -1,4 +1,4 @@
-use crate::configuration::{Configuration, Ipv6Setting};
+use crate::configuration::{Configuration, Ipv6Setting, RuleOpts};
 use crate::router::Router;
 use anyhow::{anyhow, Result};
 use log::{debug, error, info};
@@ -78,28 +78,6 @@ impl DNSRequestHandler {
             return Ok(vec![]);
         }
 
-        if request.query().query_type() == RecordType::AAAA && route.opts.ipv6 == Ipv6Setting::Defer
-            || request.query().query_type() == RecordType::A
-                && route.opts.ipv6 == Ipv6Setting::Prefer
-        {
-            let (v4, v6) = dual_stack_resolve(resolver, domain.clone()).await;
-            match route.opts.ipv6 {
-                Ipv6Setting::Defer => {
-                    if !v4.is_empty() {
-                        return Ok(v4);
-                    };
-                    return Ok(v6);
-                }
-                Ipv6Setting::Prefer => {
-                    if !v6.is_empty() {
-                        return Ok(v6);
-                    }
-                    return Ok(v4);
-                }
-                _ => {}
-            }
-        }
-
         info!(
             "DNS Request {} {} is dispatched to -> {}",
             request.query().query_type().to_string(),
@@ -107,29 +85,14 @@ impl DNSRequestHandler {
             remote
         );
 
-        let res = resolver
-            .lookup(request.query().name(), request.query().query_type())
-            .await;
-        match res {
-            Err(e) => match e.kind() {
-                ResolveErrorKind::NoRecordsFound { .. } => {
-                    debug!("No records found for {}", domain);
-                    Ok(vec![])
-                }
-                _ => Err(anyhow!(
-                    "failed to resolve {} {}: {}",
-                    request.query().query_type(),
-                    domain,
-                    e
-                )),
-            },
-            Ok(lookup) => {
-                lookup.records().iter().for_each(|r| {
-                    debug!("DNS Response for {:?}: {:?}", lookup.query(), r);
-                });
-                Ok(lookup.records().to_vec())
-            }
+        if request.query().query_type() == RecordType::AAAA && route.opts.ipv6 == Ipv6Setting::Defer
+            || request.query().query_type() == RecordType::A
+                && route.opts.ipv6 == Ipv6Setting::Prefer
+        {
+            return Ok(dual_stack_resolve(request, resolver, &route.opts, domain).await);
         }
+
+        single_resolve(request, resolver).await
     }
 }
 
@@ -177,17 +140,71 @@ impl RequestHandler for DNSRequestHandler {
     }
 }
 
+async fn single_resolve(req: &Request, resolver: &TokioAsyncResolver) -> Result<Vec<Record>> {
+    let res = resolver
+        .lookup(req.query().name(), req.query().query_type())
+        .await;
+    match res {
+        Err(e) => match e.kind() {
+            ResolveErrorKind::NoRecordsFound { .. } => {
+                debug!("No records found for {}", req.query().name());
+                Ok(vec![])
+            }
+            _ => Err(anyhow!(
+                "failed to resolve {} {}: {}",
+                req.query().query_type(),
+                req.query().name(),
+                e
+            )),
+        },
+        Ok(lookup) => {
+            lookup.records().iter().for_each(|r| {
+                debug!("DNS Response for {:?}: {:?}", lookup.query(), r);
+            });
+            Ok(lookup.records().to_vec())
+        }
+    }
+}
+
 async fn dual_stack_resolve(
+    req: &Request,
     resolver: &TokioAsyncResolver,
+    opts: &RuleOpts,
     domain: String,
-) -> (Vec<Record>, Vec<Record>) {
-    let (v4, v6) = tokio::join!(
+) -> Vec<Record> {
+    let (r1, r2) = tokio::join!(
         resolver.lookup(domain.as_str(), RecordType::A),
         resolver.lookup(domain.as_str(), RecordType::AAAA),
     );
+    let (v4, v6) = (
+        r1.map_or_else(|_| vec![], |l| l.records().to_vec()),
+        r2.map_or_else(|_| vec![], |l| l.records().to_vec()),
+    );
 
-    (
-        v4.map_or_else(|_| vec![], |l| l.records().to_vec()),
-        v6.map_or_else(|_| vec![], |l| l.records().to_vec()),
-    )
+    let is_ipv6 = req.query().query_type() == RecordType::AAAA;
+    match opts.ipv6 {
+        Ipv6Setting::Defer => {
+            if !is_ipv6 {
+                return v4;
+            }
+            if v4.is_empty() {
+                v6
+            } else {
+                vec![]
+            }
+        }
+        Ipv6Setting::Prefer => {
+            if is_ipv6 {
+                return v6;
+            }
+            if v6.is_empty() {
+                v4
+            } else {
+                vec![]
+            }
+        }
+        _ => {
+            vec![]
+        }
+    }
 }
